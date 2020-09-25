eecef7e42153f4e30ec1db78428681b203a4acbc
ho-taro <shotaro.hosoda.1921@gmail.com>
2020-09-25 09:48:36 +0900

    userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :settlements
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "Password Include both letters and numbers"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :last_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :birthdate
+  end
+
 end
