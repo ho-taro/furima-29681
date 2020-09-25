43015db1bc3344dab39c9128ca599bc20ba5859d
ho-taro <shotaro.hosoda.1921@gmail.com>
2020-09-25 12:44:07 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    # User.create(user_params)
+
+    @user = User.new(user_params)
+    # バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      # 保存されなければ、newに戻る
+      render 'new'
+    end
+  end
 
 end
