cacb4bed63a244ed4908377499576825c012f4f8
ho-taro <shotaro.hosoda.1921@gmail.com>
2020-09-26 20:18:28 +0900

    非同期通信の導入（手数料と利益）

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,15 +6,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :products_name
-    validates :description
+    validates :products_name #length: { maximum: 40 }
+    validates :description #length: #{ maximum: 1000 }
     validates :category_id, numericality: { other_than: 1 }
     validates :status_id, numericality: { other_than: 1 }
     validates :delivery_fee_id, numericality: { other_than: 1 }
     validates :shipping_place_id, numericality: { other_than: 0 }
     validates :day_to_ship_id, numericality: { other_than: 1 }
-    validates :price
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :user
-    validates :genre
   end
 end
