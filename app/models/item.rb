6d0f19380997ee89d0e609539436a095eb3f67aa
ho-taro <shotaro.hosoda.1921@gmail.com>
2020-09-28 12:34:52 +0900

    showアクションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_place
+  belongs_to_active_hash :day_to_ship
   belongs_to :user
   has_one :settlement
   has_one_attached :image
