28b31c44824c6428becbc9b57033429097b40fc2
ho-taro <shotaro.hosoda.1921@gmail.com>
2020-09-29 12:02:02 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :products_name, :description, :category_id, :status_id, :delivery_fee_id, :shipping_place_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
-end
+
 
   def set_item
     @item = Item.find(params[:id])
   end
+end
